b82b1df452c30b372ab57448abd8fcf9990294ce
Jessica Joseph <jtjos@MacBook-Air.local>
2025-07-01 20:16:04 +0100

    created components: home, greeting and header

---
+++ src/app/app.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Home} from './home/home';
+import {Header} from './header/header';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Home, Header],
+  template: `
+  <app-header/>
+  <main>
+    <app-home/>
+  </main>`,
+
+  // style main tag below to format the home component
+  styles:[
+    `
+      main {
+        padding: 16px;
+      }
+    `
+  ]
+  // styleUrl: './header.css', './home.css',
+})
+export class App {
+  protected title = 'ToDoApp';
+}
