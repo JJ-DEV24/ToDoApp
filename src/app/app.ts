0186153136b12568e92a76ab48a39597ca549061
Jessica Joseph <jtjos@MacBook-Air.local>
2025-07-02 22:56:26 +0100

    added expansion panel and datepicker from angular material view component library

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Home} from './home/home';
 import {Header} from './header/header';
+import {MatCard, MatCardContent} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [Home, Header],
+  imports: [Home, Header, MatCard, MatCardContent],
   template: `
   <app-header/>
   <main>
-    <app-home/>
-  </main>`,
+    <mat-card>
+      <mat-card-content>
+            <app-home/>
+      </mat-card-content>
+    </mat-card>
+  </main>
+  `,
 
   // style main tag below to format the home component
   styles:[
--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -1,30 +1,39 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {Task} from './task.interface';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatExpansionModule, MatInputModule, MatButtonModule, MatDividerModule, MatIconModule
+  , MatDatepickerModule],
   templateUrl: './form.html',
-  styleUrl: './form.css'
+  styleUrl: './form.css',
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Form {
+
   listOfTasks: Task[] = []
   taskForm = new FormGroup({
     title: new FormControl(''),
-    due_date: new FormControl('')
+    due_date: new FormControl("")
   })
 
   onSubmit() {
-    alert(
-      this.taskForm.value.title + '|' + this.taskForm.value.due_date
-    );
     let task: Task = {title: this.taskForm.value.title?.toString(), due_date: this.taskForm.value.due_date?.toString(),id: Math.random()}
     this.listOfTasks.push(task)
   }
 
   // protected readonly onsubmit = onsubmit;
   protected readonly console = console;
+  protected readonly FormControl = FormControl;
 }
 
